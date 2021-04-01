d62f81e9434edb8c797db630f973212c0fd4cbdc
PratikAwaik <pratikawaik125@gmail.com>
2021-04-01 12:54:02 +0530

    Removed comments

--- src/gameLogic/player.js
+++ src/gameLogic/player.js
@@ -5,18 +5,6 @@ export default class Player {
     this.board = new Gameboard(10);
   }
 
-  // allCoordinates = (array of coordinates of all ships)
-  // allDirections = (array of direction of all ships)(horizontal or vertical) [boolean array] true if isHorizontal, false otherwise
-  // placeAllShips(shipsLength, allCoordinates, allDirections) {
-  //   shipsLength.forEach((shipLength, index) => {
-  //     this.board.placeShip(
-  //       shipLength,
-  //       allCoordinates[index],
-  //       allDirections[index],
-  //     );
-  //   });
-  // }
-
   attackEnemy(enemy, hitPosition) {
     enemy.board.receiveAttack(hitPosition);
   }
